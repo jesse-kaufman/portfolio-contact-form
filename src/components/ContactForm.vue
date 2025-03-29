a363bcb684caad5ab3d6d6e23c1f717a80206235
jesse-kaufman <jesse@jessekaufman.com>
2025-03-29 17:59:42 -0500

    feat: add message preview

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -26,6 +26,22 @@
       <button type="submit" disabled>Submit</button>
     </div>
   </form>
+
+  <div v-if="hasData()" class="preview-container">
+    <strong>Message preview:</strong>
+    <div class="preview">
+      <div v-if="formData.name" class="input-item">
+        <strong>To:</strong> Jesse Kaufman
+      </div>
+      <div v-if="formData.name" class="input-item">
+        <strong>From:</strong> {{ formData.name }}
+        <span v-if="formData.email">&lt;{{ formData.email }}&gt;</span>
+      </div>
+      <div v-if="formData.phone" class="input-item">
+        <strong>Phone:</strong> {{ formData.phone }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
