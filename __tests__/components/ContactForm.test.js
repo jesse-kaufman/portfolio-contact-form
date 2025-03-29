9b9ad771d5f1a298d1d08969901e477ea0eea6a2
jesse-kaufman <jesse@jessekaufman.com>
2025-03-29 17:00:35 -0500

    feat: add additional fields to ContactForm test

--- __tests__/components/ContactForm.test.js
+++ __tests__/components/ContactForm.test.js
@@ -5,7 +5,29 @@ import ContactForm from "@/components/ContactForm.vue"
 describe("ContactForm component", () => {
   test("renders the contact form", () => {
     render(ContactForm)
-    expect(screen.getByLabelText(/your name/i)).toBeDefined()
+
+    // Check name field.
+    const nameField = screen.getByLabelText(/your name/i)
+    expect(nameField).toBeDefined()
+    expect(nameField.type).toBe("text")
+    expect(nameField.value).toBe("")
+    expect(nameField.placeholder).toBe("John Locke")
+
+    // Check email field.
+    const emailField = screen.getByLabelText(/your email/i)
+    expect(emailField).toBeDefined()
+    expect(emailField.type).toBe("text")
+    expect(emailField.value).toBe("")
+    expect(emailField.placeholder).toBe("john.locke@example.com")
+
+    // Check phone field.
+    const phoneField = screen.getByLabelText(/your phone/i)
+    expect(phoneField).toBeDefined()
+    expect(phoneField.type).toBe("tel")
+    expect(phoneField.value).toBe("")
+    expect(phoneField.placeholder).toBe("3213213214")
+
+    // Check submit button.
     expect(screen.getByRole("button", { name: /submit/i })).toBeDisabled()
   })
 })
