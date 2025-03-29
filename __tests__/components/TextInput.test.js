afe5d16d40b9b5e687335d93cd469a7557f78e29
jesse-kaufman <jesse@jessekaufman.com>
2025-03-29 16:45:32 -0500

    chore: disable max lines for tests

--- __tests__/components/TextInput.test.js
+++ __tests__/components/TextInput.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 import { describe, it, expect } from "vitest"
 import { render, fireEvent } from "@testing-library/vue"
 import TextInput from "@/components/TextInput.vue"
