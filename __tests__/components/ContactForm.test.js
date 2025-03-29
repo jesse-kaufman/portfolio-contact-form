51efb3e4f017f8a5b627b21596357da19bc2b7f2
jesse-kaufman <jesse@jessekaufman.com>
2025-03-29 17:30:03 -0500

    test: add colon after labels

--- __tests__/components/ContactForm.test.js
+++ __tests__/components/ContactForm.test.js
@@ -7,21 +7,21 @@ describe("ContactForm component", () => {
     render(ContactForm)
 
     // Check name field.
-    const nameField = screen.getByLabelText(/your name/i)
+    const nameField = screen.getByLabelText(/your name:/i)
     expect(nameField).toBeDefined()
     expect(nameField.type).toBe("text")
     expect(nameField.value).toBe("")
     expect(nameField.placeholder).toBe("John Locke")
 
     // Check email field.
-    const emailField = screen.getByLabelText(/your email/i)
+    const emailField = screen.getByLabelText(/your email:/i)
     expect(emailField).toBeDefined()
     expect(emailField.type).toBe("text")
     expect(emailField.value).toBe("")
     expect(emailField.placeholder).toBe("john.locke@example.com")
 
     // Check phone field.
-    const phoneField = screen.getByLabelText(/your phone/i)
+    const phoneField = screen.getByLabelText(/your phone:/i)
     expect(phoneField).toBeDefined()
     expect(phoneField.type).toBe("tel")
     expect(phoneField.value).toBe("")
