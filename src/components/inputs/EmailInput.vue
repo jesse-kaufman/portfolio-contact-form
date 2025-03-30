aa8e9a147d4727dc5fc21a08edb15acc8c6a99df
jesse-kaufman <jesse@jessekaufman.com>
2025-03-29 21:49:19 -0500

    feat: add EmailInput component

---
+++ src/components/inputs/EmailInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <TextInput
+    v-model="modelValue"
+    name="email"
+    label="Your Email"
+    placeholder="john.locke@example.com"
+    required
+    :validator="validateEmail"
+  />
+</template>
+
+<script setup>
+import { ref } from "vue"
+import { validateEmail } from "../../services/validation"
+import TextInput from "../base/TextInput.vue"
+
+const modelValue = ref("")
+</script>
