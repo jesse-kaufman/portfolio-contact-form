5e50cd78e68c80cbe850345b8abd0093c402cb8f
jesse-kaufman <jesse@jessekaufman.com>
2025-03-30 08:35:07 -0500

    feat: add error support to MessageInput to be consistent with other inputs

--- src/components/inputs/MessageInput.vue
+++ src/components/inputs/MessageInput.vue
@@ -6,8 +6,10 @@
     name="message"
     placeholder="Type your message here."
     required
+    :class="{ 'input-error': error }"
     @input="updateValue"
   ></textarea>
+  <div v-if="error" class="error-message">{{ errorMessage }}</div>
 </template>
 
 <script>
@@ -17,10 +19,10 @@
  * @throws {Error} If validation fails.
  */
 export const validateMessage = (msg) => {
-  // Check that name is between 2 and 254 characters.
+  // Check that message is at least 10 characters.
   // eslint-disable-next-line no-magic-numbers
   if (msg.length < 10) {
-    throw Error("Name must be at least 10 characters long.")
+    throw Error("Message must be at least 10 characters long.")
   }
 }
 </script>
@@ -35,10 +37,24 @@ const props = defineProps({
 // Emits definition
 const emit = defineEmits(["update:modelValue"])
 const modelValue = ref(props.modelValue)
+const error = ref(false) // Error state
+const errorMessage = ref("") // Error message
 
 const updateValue = (event) => {
   const newValue = event.target.value
 
-  emit("update:modelValue", newValue)
+  // Reset error state.
+  error.value = false
+  errorMessage.value = ""
+
+  try {
+    validateMessage(newValue)
+    // Emit update event if valid.
+    emit("update:modelValue", newValue)
+  } catch (err) {
+    // Set error state and message.
+    error.value = true
+    errorMessage.value = err.message
+  }
 }
 </script>
