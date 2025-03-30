0b89d1b4f5eed51c3cfaf61917226016e3e8c187
jesse-kaufman <jesse@jessekaufman.com>
2025-03-29 21:54:21 -0500

    feat: add PhoneInput component

---
+++ src/components/inputs/PhoneInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <TextInput
+    v-model="modelValue"
+    name="phone"
+    label="Your Phone"
+    placeholder="3213213214"
+    required
+    :validator="validatePhone"
+  />
+</template>
+
+<script setup>
+import { ref } from "vue"
+import { validatePhone } from "../../services/validation"
+import TextInput from "../base/TextInput.vue"
+
+const modelValue = ref("")
+</script>
