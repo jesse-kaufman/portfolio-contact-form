a85da5d30c6bc88de82f35a6f994d0075b64edb7
jesse-kaufman <jesse@jessekaufman.com>
2025-03-29 21:45:46 -0500

    test: add tests for EmailInput component

---
+++ __tests__/components/inputs/EmailInput.test.js
@@ -0,0 +1,35 @@
+/** @file EmailInput component tests. */
+
+import { describe, it, expect } from "vitest"
+import { render, fireEvent } from "@testing-library/vue"
+import EmailInput from "@/components/inputs/EmailInput.vue"
+
+describe("EmailInput.vue", () => {
+  // Tests for component rendering.
+  describe("rendering", () => {
+    it("renders the input with the correct label", () => {
+      const { getByLabelText } = render(EmailInput)
+      expect(getByLabelText("Your Name:")).toBeInTheDocument()
+    })
+  })
+
+  // Tests for component interaction.
+  describe("interaction", () => {
+    it("binds the value correctly with v-model", async () => {
+      const { getByLabelText } = render(EmailInput)
+      const input = getByLabelText("Your Email:")
+      await fireEvent.update(input, "john.lock@example.com")
+      expect(input.value).toBe("john.lock@example.com")
+    })
+  })
+
+  // Tests for component validation.
+  describe("validation", () => {
+    it("validates value correctly with v-model", async () => {
+      const { getByText, getByLabelText } = render(EmailInput)
+      const input = getByLabelText("Your Email:")
+      await fireEvent.update(input, "J")
+      expect(getByText("Invalid email address.")).toBeInTheDocument()
+    })
+  })
+})
