8459f0d416ec0b8f1def48a0ab6ebe1142c28216
jesse-kaufman <jesse@jessekaufman.com>
2025-03-29 19:55:10 -0500

    chore: add @file to beginning of file

--- __tests__/components/TextInput.test.js
+++ __tests__/components/TextInput.test.js
@@ -1,3 +1,4 @@
+/** @file TextInput component tests. */
 /* eslint-disable max-lines-per-function */
 import { describe, it, expect } from "vitest"
 import { render, fireEvent } from "@testing-library/vue"
