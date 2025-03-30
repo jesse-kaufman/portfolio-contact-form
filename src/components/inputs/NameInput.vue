49749e6f26bef2e690b01ec63b7cb30f3a774a7b
jesse-kaufman <jesse@jessekaufman.com>
2025-03-29 21:33:21 -0500

    feat: add NameInput.vue for name

---
+++ src/components/inputs/NameInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <TextInput
+    v-model="modelValue"
+    name="name"
+    label="Your Name"
+    placeholder="John Locke"
+    required
+    :validator="validateName"
+  />
+</template>
+
+<script setup>
+import { ref } from "vue"
+import { validateName } from "../../services/validation"
+import TextInput from "../base/TextInput.vue"
+
+const modelValue = ref("")
+</script>
