4878a6591b45843133a01c1eead1a23917631367
jesse-kaufman <jesse@jessekaufman.com>
2025-03-29 21:34:29 -0500

    fix: use modelValue for v-model name

--- src/components/inputs/MessageInput.vue
+++ src/components/inputs/MessageInput.vue
@@ -2,7 +2,7 @@
   <label for="message">Message:</label>
   <textarea
     id="message"
-    v-model="inputValue"
+    v-model="modelValue"
     name="message"
     placeholder="Type your message here."
     required
@@ -19,7 +19,7 @@ const props = defineProps({
 
 // Emits definition
 const emit = defineEmits(["update:modelValue"])
-const inputValue = ref(props.modelValue)
+const modelValue = ref(props.modelValue)
 
 const updateValue = (event) => {
   const newValue = event.target.value
